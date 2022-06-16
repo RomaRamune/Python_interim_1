d73ff14553bcccffef40e3dea33a9955243036b2
RomaRamune <50999416+RomaRamune@users.noreply.github.com>
2022-06-17 00:25:29 +0300

    Task5 completed plus module
    Task5 and module so the taks would work

---
+++ mymodule.py
@@ -0,0 +1,28 @@
+from math import *
+
+def addition(one, four):
+    return 1 + 4
+
+def division(four, two):
+    return 4 / 2
+
+def substraction(three, two):
+    return 3 - 2
+
+def multiplication(five, two):
+    return 5 * 2
+
+def one(one):
+    return 1
+
+def two(two):
+    return 2
+
+def three(three):
+    return 3
+
+def four(four):
+    return 4
+#
+def five(five):
+    return 5
